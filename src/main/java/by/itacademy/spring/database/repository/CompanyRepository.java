c51a937be568b82877a26b41a4616cabf880af42
Andrei Borisov <andrei.borr@gmail.com>
2023-05-19 19:34:31 +0300

    spring-2 @Autowired

--- src/main/java/by/itacademy/spring/database/repository/CompanyRepository.java
+++ src/main/java/by/itacademy/spring/database/repository/CompanyRepository.java
@@ -1,12 +1,18 @@
 package by.itacademy.spring.database.repository;
 
-import by.itacademy.spring.bpp.InjectBean;
 import by.itacademy.spring.database.pool.ConnectionPool;
+import jakarta.annotation.Resource;
 import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @ToString
 public class CompanyRepository {
+    private ConnectionPool connectionPool1;
 
-    @InjectBean
-    private ConnectionPool connectionPool;
+    @Autowired
+    private List<ConnectionPool> pools;
 }
